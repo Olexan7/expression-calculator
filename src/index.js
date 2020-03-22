ae1697fb33c8aaf340584fcb77289827254a1979
Alexandr Kondratiev <ghoostt61@gmail.com>
2020-03-22 23:11:25 +0400

    feat: add solution expression calculator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.replace(/ /g, "");
+  if (expr.indexOf("/0") != -1) throw new Error("TypeError: Division by zero.");
+  let open = 0;
+  let close = 0;
+  expr.split("").forEach(element => {
+    if (element === "(") open++;
+    else if (element == ")") close++;
+  });
+  if (open != close)
+    throw new Error("ExpressionError: Brackets must be paired");
+
+  let answer = new Function("return " + expr);
+  return answer();
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator
+};
